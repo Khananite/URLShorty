3e4eae20c3c22530e8d001b81e156611fae70beb
Khananite <53943019+Khananite@users.noreply.github.com>
2022-09-02 14:10:58 +0100

    Update previewUrlShortenerInteractor.js

--- client_side/assets/js/URLShortenerSection/previewUrlShortenerInteractor.js
+++ client_side/assets/js/URLShortenerSection/previewUrlShortenerInteractor.js
@@ -15,38 +15,9 @@ $(function()
 
 async function initPreviewUrlShortenerContract()
 {
-  if (window.ethereum)
-  {
-    try
-    {
-      //Check whether user has an address connected to metamask.
-      //'eth_accounts' returns an array that is either empty or contains a single account address.
-      //The "currently selected" address is the first item in the array returned by 'eth_accounts'.
-      //https://docs.metamask.io/guide/ethereum-provider.html#table-of-contents
-      const userWalletAddresses = await ethereum.request({method: 'eth_accounts'});
-    
-      //Check if user has an account connected to metamask.
-      if(userWalletAddresses.length > 0)
-      {
-        userWalletAddress = userWalletAddresses[0];
-  
-        //Create new instance of the PreviewURLShortener smart contract.
-        //previewUrlShortenerAbi is a variable, which is stored in abiPreviewUrlShortener.js
-        previewUrlShortenerObject = new web3.eth.Contract(previewUrlShortenerAbi, previewUrlShortenerContractAddress);
-      }
-    }
-    catch(error)
-    {
-      if (error.code === 4001)
-      {
-        console.log('User rejected ethereum accounts request');
-      }
-    }
-  }
-  else
-  {
-    console.log('window.ethereum not available');
-  }
+  //Create new instance of the PreviewURLShortener smart contract.
+  //previewUrlShortenerAbi is a variable, which is stored in abiPreviewUrlShortener.js
+  previewUrlShortenerObject = new web3.eth.Contract(previewUrlShortenerAbi, previewUrlShortenerContractAddress);
 }
 
 async function previewShortenedUrl()
@@ -87,4 +58,3 @@ async function previewShortenedUrl()
     $('#submitShortUrl_button').prop('disabled', false);
   }
 }
-
